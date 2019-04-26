2dd1d12803219b6c8619b83233fb739ad8fbd9a0
M-Wellerson <m_wellerson@outlook.com>
2019-04-26 12:32:13 -0300

    bug corrigido

--- tema/start/disc/js/main.js
+++ tema/start/disc/js/main.js
@@ -48,6 +48,7 @@ fetch(app)
                     });
                 });
                 let cont = 0;
+                document.querySelector('#data__atual').value = dataSemana;
                 document.querySelector('#reserva__horarios').innerHTML = '<div>Data</div><div>Horário / Dia da Semana</div>' + horario.map(h => `<div>${h.inicio} - ${h.final}</div>`).sort().join('');
                 document.querySelector('#agenda_reserva').innerHTML = agenda.map(a => `<label id="lb_${a}" onclick="setHorario( '${a}' )" for="pop-agenda-livre"><div class="agenda-disponivel" id="agenda_${a}">Disponivel</div></label>`).join('');
                 document.querySelector('#agenda_semana').innerHTML = week.map(a => `<div id="lb_A-${cont++}-">${a.split('@')[0].split('-').reverse().join('/')}</div>`).join('');
